aaeadbe9b87dd1c7e2a3b17a369c0d9b6bcddb0e
RodrigoWaechter <rodrigowaechter@systemhaus.com.br>
2024-07-06 17:00:11 -0300

    Setup

---
+++ src/main/java/com/projeto/agenda/client/AgendaClientApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.agenda.Agenda_Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaClientApplication.class, args);
+	}
+
+}
